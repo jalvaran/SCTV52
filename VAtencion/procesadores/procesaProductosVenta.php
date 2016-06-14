063e815d10d7443665cbcdcfd4b9561160585a91
jalvaran <jalvaran@gmail.com>
2016-06-14 14:45:24 -0500

    impresion de codigos de barra listo2

--- VAtencion/procesadores/procesaProductosVenta.php
+++ VAtencion/procesadores/procesaProductosVenta.php
@@ -12,6 +12,8 @@ if(isset($_REQUEST["BtnImprimirBarCode"])){
     $Tabla="productosventa";
     $DatosCB["EmpresaPro"]=1;
     $DatosPuerto=$obVenta->DevuelveValores("config_puertos", "ID", 2);
+    if($DatosPuerto["Habilitado"]=="SI"){
+        $obVenta->ImprimirCodigoBarras($Tabla,$idProducto,$Cantidad,$DatosPuerto["Puerto"],$DatosCB);
+    }
     
-    $obVenta->ImprimirCodigoBarras($Tabla,$idProducto,$Cantidad,$DatosPuerto["Puerto"],$DatosCB);
 }
